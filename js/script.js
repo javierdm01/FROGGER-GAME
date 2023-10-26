1a352535572c31871bf6809afb91c7ef9b870cc8
javierdm01 <diazmolinajavier04@gmail.com>
2023-10-26 19:58:17 +0200

    Movimiento de Rana

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 //Declaración de variables
 let score=document.getElementById('score');
 let game=document.getElementById('game');
-let frogg=document.getElementById('frogg')
+//Posicion de la Rana
+let ranaX=400;
+let ranaY=730;
 
 //Generar Página
 const generarPagina=(e)=>{
@@ -9,7 +11,6 @@ const generarPagina=(e)=>{
     game.append(generarObjetos('H1','tit__game','FROGGER'));
     game.append(generarObjetos('H2','subtit__game','PRESS ANY KEY TO START'))
 }
-
 
 //Función para generar Objetos
 const generarObjetos=(elemento,clase,txt)=>{
@@ -33,11 +34,79 @@ const generarMapa=()=>{
     fragment.append(generarObjetos('DIV','cesped__panel'));
     game.appendChild(fragment)
 }
-
-const inciarJuego=()=>{
-    
+const generarVias=()=>{
+    //Generar Vías
+    let fragment=document.createDocumentFragment()
+    fragment.append(generarObjetos('DIV','via__carretera_primer_via'))
+    fragment.append(generarObjetos('DIV','via__carretera'))
+    fragment.append(generarObjetos('DIV','via__carretera'))
+    fragment.append(generarObjetos('DIV','via__carretera'))
+    fragment.append(generarObjetos('DIV','via__carretera'))
+    game.children[3].append(fragment)
+    //Generar rio
+    let fragment_rio=document.createDocumentFragment()
+    fragment_rio.append(generarObjetos('DIV','via__rio'))
+    fragment_rio.append(generarObjetos('DIV','via__rio'))
+    fragment_rio.append(generarObjetos('DIV','via__rio'))
+    fragment_rio.append(generarObjetos('DIV','via__rio'))
+    fragment_rio.append(generarObjetos('DIV','via__rio'))
+    game.children[1].append(fragment_rio)
 }
+const generarObstaculos=()=>{
+    let frogg=document.createElement('DIV')
+    frogg.classList.add('frogg_img')
+    frogg.id='frogg'
+    frogg.style.left=ranaX+'px';
+    frogg.style.top=ranaY+'px';
+    game.children[4].append(frogg);
+}
+const inciarJuego=(e)=>{
+    game.children[game.children.length-1].remove();
+    game.children[game.children.length-1].remove();
+    generarVias();
+    generarObstaculos();
+    document.removeEventListener(e.type,inciarJuego);
+}
+const moverRana=(e)=>{
+    switch (e.key) {
+        case 'ArrowRight':
+            if(ranaX<720){
+                ranaX+=50
+                frogg.style.left=ranaX +'px'
+            }
+        break;
+        case 'ArrowLeft':
+            if(ranaX>0){
+                ranaX-=50
+                frogg.style.left=ranaX +'px'
+            }
+        break;
+        case 'ArrowUp':
+            if(ranaY>10){
+                ranaY-=60 
+                frogg.style.top=ranaY +'px'
+            }
+        break;
+        case 'ArrowDown':
+            if(ranaY<720){
+                ranaY+=60 
+                frogg.style.top=ranaY +'px'
+            }
+        break;
+        default:
+            break;
+    } 
+}
+const moverJuego=(e)=>{
+    moverRana(e);
+    
+       
+}
+
 //Evento Generar Página
 document.addEventListener("DOMContentLoaded",generarPagina)
 //Evento Empezar a Jugar
 document.addEventListener('keydown',inciarJuego)
+//
+//Posicion de rana
+document.addEventListener('keydown',moverJuego)
